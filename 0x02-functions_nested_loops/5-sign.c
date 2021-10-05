17c00c3787f141854173e0ffac6434912856f669
Luis Ernesto <noahverner@outlook.com>
2021-10-05 03:35:56 -0500

    This C Program prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Check if is a letter, lowercase or uppercase *
+ * @n: is the representation of char in ascii code
+ *
+ * Return: 1 if letter else 0 
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
