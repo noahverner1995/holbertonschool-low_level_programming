a28e08d0b6a21cdfff10e61d0428207392a7b97b
Luis Ernesto <noahverner@outlook.com>
2021-10-05 02:42:04 -0500

    This C Program prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/**
+ * main - Writes the string Holberton to stdout
+ *
+ * Return: On success 0
+ */
+int main(void)
+{
+char c[] = "_putchar";
+int i;
+
+for (i = 0; i < 9; i++)
+_putchar(c[i]);
+_putchar('\n');
+
+return (0);
+}
