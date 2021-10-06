1b3736dd66501b15e93d32548e2a0540b48c5a29
Luis Ernesto <noahverner@outlook.com>
2021-10-05 23:56:52 -0500

    This C Program correctly prints out the largest of three numbers, no matter the case

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers no matter the case
  * @a: first integer
  * @b: second integer
  * @c: third integer
