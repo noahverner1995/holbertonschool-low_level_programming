6e4aa19f1a31a67240cc6830b947834cf7727618
Luis Ernesto <noahverner@outlook.com>
2021-10-08 22:57:54 -0500

    This C program  checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * _isupper - Check for uppercase character
+ *
+ * @c: Integer parameter
+ * Return: 1 if uppercase character else 0
+ */
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+return (1);
+else
+return (0);
+}
