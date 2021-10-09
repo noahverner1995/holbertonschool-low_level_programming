297cca47aaf64526ed661128578b3b94acbde99b
Luis Ernesto <noahverner@outlook.com>
2021-10-08 23:24:53 -0500

    This C program prints the numbers, from 0 to 9, ignoring 2 and 4 followed by a new line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers from 0 to 9
+ * except 2 and 4
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+
+for (i = 0; i < 10; ++i)
+{
+if (i != 2 && i != 4)
+_putchar(i + '0');
+}
+_putchar('\n');
+}
