2d64e429bd5297893b6c5001d64875cef838543d
Luis Ernesto <noahverner@outlook.com>
2021-10-11 22:56:52 -0500

    This C program prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Print a string in reverse, followed by a newline
+ *
+ * @str: Pointer of char variable
+ */
+void print_rev(char *str)
+{
+char *t;
+int counter = 0;
+int i;
+
+for (t = str; *t != '\0'; t++)
+counter++;
+
+
+for (i = counter - 1; i >= 0; i--)
+_putchar(*(str + i));
+
+_putchar('\n');
+}
