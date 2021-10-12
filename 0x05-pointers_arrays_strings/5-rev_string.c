7e6c3ff290c4f5bf84d52c73f8f7cd0fb3f6bc9e
Luis Ernesto <noahverner@outlook.com>
2021-10-11 23:00:00 -0500

    This C program reverses a string in the same memory allocation

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string in the same memory location
+ *
+ * @s: Pointer of char variable
+ */
+void rev_string(char *s)
+{
+char *t;
+int counter = 0, middle, temp, i, j, last, begin;
+
+for (t = s; *t != '\0'; t++)
+counter++;
+
+middle = (counter % 2 == 0) ? (counter / 2) : ((counter - 1) / 2);
+j = counter - 1;
+for (i = 0; i < middle; i++)
+{
+last = s[j];
+begin = s[i];
+temp = last;
+s[j] = begin;
+s[i] = temp;
+j--;
+}
+}
