2e6274d6b47145305198d2148450d3cfcdf90be1
Luis Ernesto <noahverner@outlook.com>
2021-10-11 23:23:22 -0500

    This C program prints half right of a string, followed by a new line

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half-right of a string
+ *
+ * @str: Pointer of char[] variable
+ */
+void puts_half(char *str)
+{
+char *t;
+int counter = 0, middle;
+int i;
+
+for (t = str; *t != '\0'; t++)
+counter++;
+
+if (counter % 2 != 0)
+{
+middle = ((counter - 1) / 2);
+middle++;
+}
+else
+{
+middle = counter / 2;
+}
+for (i = middle; i < counter; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
