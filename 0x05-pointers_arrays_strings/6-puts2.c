4e3eaa51dcc329f341a778be256e3e93f5f4fa00
Luis Ernesto <noahverner@outlook.com>
2021-10-11 23:09:36 -0500

    This C program prints every other character of a string, starting with the first character, followed by a new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Print the even position of a string
+ *
+ * @str: Pointer of char[] variable
+ */
+void puts2(char *str)
+{
+char *t;
+int counter = 0;
+int i;
+
+for (t = str; *t != '\0'; t++)
+counter++;
+
+for (i = 0; i < counter; i += 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
