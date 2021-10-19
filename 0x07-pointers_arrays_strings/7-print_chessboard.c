38b44ff66d25440715cd84bca465348feeee677b
Luis Ernesto <noahverner@outlook.com>
2021-10-18 23:16:37 -0500

    This C program has a function that prints a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a: char pointer of two dimensions
+ *
+ * Return: Print a chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c", a[i][j]);
+}
+printf("\n");
+}
+}
