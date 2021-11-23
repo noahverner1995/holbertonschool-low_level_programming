4bf4599243659ba2ff095ef48dfe16ad7d6e49bb
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2021-11-23 01:58:46 -0500

    This C program appends text at the end of a file
    -where "filename" is the name of the file and "text_content" is the "NULL" terminated string to add at the end of the file
    -return: "1" on success and "-1" on failure
    -do not create the file if it does not exist
    -if "filename" is "NULL" return "-1"
    -if "text_content" is "NULL", do not add anything to the file. Return "1" if the file exists and "-1" if the file does not exist or if you do not have the required permissions to write the file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file and write in it
- * @filename: Name of the file created
- * @text_content: Content to be written
+ * append_text_to_file - Appends text at the end of a file
+ * @filename: Name of the file
+ * @text_content: Content to append
  *
  * Return: 1 on success, -1 on failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, rd, i;
 char *p = text_content;
 
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+fd = open(filename, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
+
 if (fd == -1)
 return (-1);
+
 if (text_content != NULL)
 {
 for (i = 0; p[i] != '\0'; i++)
@@ -27,8 +29,5 @@ if (rd == -1)
 return (-1);
 }
 
-close(fd);
-
 return (1);
-
 }
